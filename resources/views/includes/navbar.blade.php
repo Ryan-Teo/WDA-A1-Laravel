ceb668291411887591b9d62417b51efa4d6e4af9
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2017-08-22 01:25:09 +1000

    navbar formatting

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,7 @@
 <div class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
+        <a class="navbar-brand" href="{{ Request::root() }}"><img src="{{ url('img/ITS Logo.png') }}" alt="ITS Logo goes here">
+        </a>
         <div class="navbar-header navbar-right">
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
                 <span class="sr-only">Toggle navigation</span>
@@ -10,8 +12,6 @@
         </div>
         <div class="navbar-collapse collapse" id="navbar-main">
             <ul class="nav navbar-nav ">
-                <a class="navbar-brand" href="{{ Request::root() }}"><img src="{{ url('img/ITS Logo.png') }}" alt="ITS Logo goes here">
-                </a>
                 <li><a href="{{ url('/users/create') }}">Create Account</a></li>
                 <li><a href="{{ url('/inquiries/create') }}">Send enquiry</a></li>
                 <li><a href="{{ url('/inquiries/index') }}">Track Progress</a></li>
--- resources/views/inquiries/form.blade.php
+++ resources/views/inquiries/form.blade.php
@@ -1,32 +1,34 @@
 @extends('layouts.master')
 @section('content')
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Comments:</strong>
-                {!! Form::textarea('comments', null, array('placeholder' => 'Details',
-                'class' => 'form-control','style'=>'height:100px')) !!}
+        {!! Form::close() !!}    {!! Form::model($inquiry, ['method' => 'PATCH','route' => ['inquiries.update', $inquiry->id]]) !!}
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Comments:</strong>
+                    {!! Form::textarea('comments', null, array('placeholder' => 'Details',
+                    'class' => 'form-control','style'=>'height:100px')) !!}
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
 
-                {{ Form::radio('status', 'pending') }}
-                {!! Form::label('status', 'Pending') !!}
-                <br>
-                {{ Form::radio('status', 'resolved') }}
-                {!! Form::label('status', 'Resolved') !!}
-                <br>
-                {{ Form::radio('status', 'unresolved') }}
-                {!! Form::label('status', 'Unresolved') !!}
-                <br>
-                {{ Form::radio('status', 'closed') }}
-                {!! Form::label('status', 'Closed') !!}
+                    {{ Form::radio('status', 'pending') }}
+                    {!! Form::label('status', 'Pending') !!}
+                    <br>
+                    {{ Form::radio('status', 'resolved') }}
+                    {!! Form::label('status', 'Resolved') !!}
+                    <br>
+                    {{ Form::radio('status', 'unresolved') }}
+                    {!! Form::label('status', 'Unresolved') !!}
+                    <br>
+                    {{ Form::radio('status', 'closed') }}
+                    {!! Form::label('status', 'Closed') !!}
+                </div>
+
             </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        {!! Form::close() !!}
     </div>
 @endsection
