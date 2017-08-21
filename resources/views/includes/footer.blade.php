f87101fcaa8a7f69754a9051c9878e7cb7e8538b
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2017-08-21 23:08:01 +1000

    added some footer formatting

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,12 @@
 <footer class="footer">
-    <p>&copy; Company 2017</p>
+    <div class="row navbar-fixed-bottom">
+        <div class="col-lg-6 pull-left">
+            <p>&copy; ABCD Company 2017</p>
+        </div>
+        <div class="col-lg-6 pull-right">
+            <p>Made by : Harry Utomo & Ryan Teo</p>
+        </div>
+    </div>
 </footer>
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,21 +6,19 @@
 
 @section('content')
     <div class="container">
-        <div class="container">
-
             <div class="page-header" id="banner">
                 <div class="row">
                     <div class="col-lg-6">
                         <h1>Welcome</h1>
-                        <p class="lead">To a new RMIT ITS Online Support Website.</p>
+                        <p class="lead">To the new RMIT ITS Online Support Website.</p>
                     </div>
                 </div>
             </div>
-        </div>
 
         <div class ="row">
             <div class ="col-md-4">
-                <h2>What we do</h2>
+                <h2>What we do.</h2>
+                <p>We solve all your techincal problems. </p>
                 <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
                 <p><small>This line of text is meant to be treated as fine print.</small></p>
                 <p>The following snippet of text is <strong>rendered as bold text</strong>.</p>
@@ -53,6 +51,5 @@
             <p><a href="{{ url('inquiries/create') }}" class="btn btn-primary btn-lg">Click me</a></p>
         </div>
     </div>
-
-    @stop
+@endsection
 
